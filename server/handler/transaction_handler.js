d8029b429985f99fb42783653c91e7f8e37ed2a1
vdas9de <Welcome6>
2016-11-21 18:47:54 +0530

    Handler added for transaction

---
+++ server/handler/transaction_handler.js
@@ -0,0 +1,44 @@
+/**
+ * Created by debayan.das on 17-10-2016.
+ */
+(function(){
+    var transactionSchema = require("./../schema/transaction");
+    function TransactionHandler(){}
+    TransactionHandler.prototype.saveTransaction = function(req, res){
+        try{
+            var groupieId = parseInt(req.params.groupieId,10),
+                transactionDetails = req.body,
+                transaction = new transactionSchema();
+            transaction.transactionDate = transactionDetails.transactionDate;
+            transaction.description = transactionDetails.description;
+            transaction.payee = transactionDetails.payee;
+            transaction.participants = transactionDetails.participants;
+            transaction.cost = transactionDetails.cost;
+            transaction.groupieid = groupieId;
+            // Saving it to the database.
+            transaction.save(function (err) {
+                if (err){
+                    console.error(err);
+                    console.log('Error on save!');
+                    res.send({
+                        transactionId : "error on save"
+                    });
+                    res.end();
+                    return;
+                }
+                console.log("saved successfully");
+                res.send({
+                    transactionId : transaction.transactionid
+                });
+                res.end();
+                
+            });
+        }catch(e){
+            console.error("error saving transacrion");
+            console.error(req.body);
+            res.send("saved");
+            res.end();
+        }
+    }
+    module.exports =  new TransactionHandler();
+}());
--- server/schema/transaction.js
+++ server/schema/transaction.js
@@ -3,27 +3,66 @@
  */
 (function () {
     var mongoose = require("mongoose"),
+        user = require("./user"),
         Schema = mongoose.Schema,
         transactionSchema = new Schema({
-            transactionDate: {type: Date, required: true},
+            transactionDate: {type: String, required: true},
             transactionid: {type: Number, unique: true},
             description: {type: String},
             cost: {type: Number, required: true},
-            payee: [{type: String, required: true}],
+            payee: [{type: Number, required: true}],
             participants: [{
-                name: String,
-                contribution: Number
+                type: Number
             }],
-            groupieid: {type: String, required: true}
+            groupieid: {type: Number, required: true}
         });
 
-    transactionSchema.pre('save', function(next) {
-        console.log("hochhe to!!!");
-        mongoose.connection.db.eval('getNextSequenceValue("transactionid")', (function(err, returnValue){
-            this.transactionid = returnValue;
-            next();
+    transactionSchema.pre('save', function (next) {
+        console.log("on pre")
+        this.updateUser((function () {
+            if (this.transactionid) {
+                next();
+            } else {
+                this.getTransactionId(next);
+            }
         }).bind(this));
     });
+
+    transactionSchema.methods.getTransactionId = function (callback) {
+        mongoose.connection.db.eval('getNextSequenceValue("transactionid")', (function (err, returnValue) {
+            this.transactionid = returnValue;
+            callback();
+        }).bind(this));
+    };
+
+    transactionSchema.methods.updateUser = function (callback) {
+        var cost = this.cost,
+            noOfParticipants = this.participants.length;
+        var costPerParticipant = cost / noOfParticipants;
+        for (var i = 0; i < this.participants.length; i++) {
+            var currentParticipant = this.participants[i],
+                participantCost = this.payee.indexOf(currentParticipant) > -1 ? cost - costPerParticipant : -costPerParticipant;
+            (function (participantCost, context) {
+                user.findOne({"userid": currentParticipant}, (function (err, userObj) {
+                    var userGroups = userObj.groupies;
+                    var currentGroup;
+                    for (var i = 0; i < userGroups.length; i++) {
+                        if (userGroups[i].groupieid === this.groupieid) {
+                            currentGroup = userGroups[i];
+                            break;
+                        }
+                    }
+                    currentGroup.outstanding += participantCost;
+                    userObj.save(function (err) {
+                        if (err) console.log('Error on save transaction!');
+                        console.log("transaction saved successfully");
+                    });
+                }).bind(context));
+            }(participantCost, this));
+
+        }
+        callback();
+    };
 
     module.exports = mongoose.model("transaction", transactionSchema);
 }());
